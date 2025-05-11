046d86b8aafac65fb9decb253a111b1e8bfda50c
Antonytm <antontishchenko@gmail.com>
2025-05-11 03:11:36 +0300

    feat: security-enable-user

---
+++ src/tools/powershell/simple/security/register-enable-user.ts
@@ -0,0 +1,23 @@
+import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
+import type { Config } from "../../../../config.js";
+import { z } from "zod";
+import { safeMcpResponse } from "../../../../helper.js";
+import { runGenericPowershellCommand } from "../generic.js";
+
+export function registerEnableUserPowerShell(server: McpServer, config: Config) {
+    server.tool(
+        "security-enable-user",
+        "Enables the Sitecore user account.",
+        {
+            identity: z.string(),
+        },
+        async (params) => {
+            const command = `Enable-User`;
+            const options: Record<string, any>= {
+                "Identity": params.identity,
+            };
+
+            return safeMcpResponse(runGenericPowershellCommand(config, command, options));
+        }
+    );
+}
