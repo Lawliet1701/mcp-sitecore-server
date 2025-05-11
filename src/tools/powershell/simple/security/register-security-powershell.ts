44457f1ceea42647385ebb6252894fec6b452b4e
Antonytm <antontishchenko@gmail.com>
2025-05-11 03:06:09 +0300

    feat: security-disable-user

--- src/tools/powershell/simple/security/register-security-powershell.ts
+++ src/tools/powershell/simple/security/register-security-powershell.ts
@@ -6,9 +6,11 @@ import { register } from "module";
 import { registerGetUserPowerShell } from "./register-get-user.js";
 import { registerNewUserPowerShell } from "./register-new-user.js";
 import { registerRemoveUserPowerShell } from "./register-remove-user.js";
+import { registerDisableUserPowerShell } from "./register-disable-user.js";
 
 export function registerSecurityPowerShell(server: McpServer, config: Config) {
     registerGetUserPowerShell(server, config);
     registerNewUserPowerShell(server, config);
     registerRemoveUserPowerShell(server, config);
+    registerDisableUserPowerShell(server, config);
 }
