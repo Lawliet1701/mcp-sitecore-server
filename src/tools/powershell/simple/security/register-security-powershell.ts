e3f633ae18fae2bcd446b42d8e433e7e51279c01
Antonytm <antontishchenko@gmail.com>
2025-05-11 17:29:22 +0300

    fix: remove unused imports

--- src/tools/powershell/simple/security/register-security-powershell.ts
+++ src/tools/powershell/simple/security/register-security-powershell.ts
@@ -1,8 +1,5 @@
 import type { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import type { Config } from "../../../../config.js";
-import { z } from "zod";
-import { safeMcpResponse } from "../../../../helper.js";
-import { register } from "module";
 import { registerGetUserPowerShell } from "./register-get-user.js";
 import { registerNewUserPowerShell } from "./register-new-user.js";
 import { registerRemoveUserPowerShell } from "./register-remove-user.js";
