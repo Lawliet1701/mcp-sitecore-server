6419e15e12063b495420affac43f1e3f5ee32cdf
Stanislav Chernetsky <schernetsky@uxbee.nl>
2025-06-23 12:58:15 +0300

    Allow to pass hashtable parameters

---
+++ src/tools/powershell/command-builder.ts
@@ -0,0 +1,76 @@
+import { PowershellVariable } from "./variable.js";
+
+export class PowershellCommandBuilder
+{
+    buildCommandString(script: string, parameters: Record<string, any> = {}): string {
+        let scriptWithParameters = script;
+        if (parameters) {
+            for (const parameter in parameters) {
+                if (parameters[parameter] === "") {
+                    scriptWithParameters += ` -${parameter}`;
+                }
+                else if (Array.isArray(parameters[parameter])) {
+                    scriptWithParameters += ` -${parameter} "${parameters[parameter].join('","')}"`;
+                }      
+                else if (parameters[parameter] instanceof PowershellVariable)
+                {
+                    scriptWithParameters += ` -${parameter} ${parameters[parameter].getValue()}`;
+                }
+                else if (this.isRecord(parameters[parameter])) {
+                    // Check whether the record has any keys
+                    if(Object.getOwnPropertyNames(parameters[parameter]).length > 0)
+                    {
+                        scriptWithParameters += ` -${parameter} ${this.buildPowershellHashtableString(parameters[parameter])}`;
+                    }                    
+                }          
+                else {
+                    scriptWithParameters += ` -${parameter} "${parameters[parameter]}"`;
+                }
+            }
+        }
+
+        return scriptWithParameters;
+    }
+
+    private buildPowershellHashtableString(parameters: Record<string, any>): string {
+        let result = "@{ ";
+        let first = true;
+        for (const parameter in parameters) {
+            if (!first)
+            {
+                result += "; ";
+            }
+    
+            result += `"${parameter}" = "${parameters[parameter]}"`;
+            first = false;
+        }
+    
+        result += " }";
+    
+        return result;
+    }
+    
+    private isRecord(value: any): boolean {
+        if (!value)
+        {
+            return false;
+        }
+    
+        if (typeof value !== "object")
+        {
+            return false;
+        }
+    
+        if (Array.isArray(value))
+        {
+            return false;
+        }
+    
+        if (Object.getOwnPropertySymbols(value).length > 0)
+        {
+            return false;
+        }
+          
+        return Object.getOwnPropertyNames(value).every(prop => typeof value[prop] === "string")
+    }
+}
